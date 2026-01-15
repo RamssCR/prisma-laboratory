53dd750daea55e947b25c26d31e15164065bdbaa
Alejandro Molina <alejandro.ytf@gmail.com>
2026-01-15 16:41:02 -0500

    feat: controlador update y delete de usuario

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,11 @@
-import { createUser, getUsers } from '#controllers/user';
+import {
+  createUser,
+  deleteUser,
+  getUsers,
+  updateUser,
+} from '#controllers/user';
 import { validate } from '#middlewares/schema';
+import { idParams } from '#schemas/idParam';
 import { userSchema } from '#schemas/user';
 import { Router } from 'express';
 
@@ -7,3 +13,12 @@ export const router = Router();
 
 router.post('/register', validate(userSchema), createUser);
 router.get('/', getUsers);
+router.patch(
+  '/:email',
+  [
+    validate(userSchema, { target: 'params', mode: 'partial' }),
+    validate(userSchema, { mode: 'partial' }),
+  ],
+  updateUser,
+);
+router.delete('/:id', validate(idParams, { target: 'params' }), deleteUser);
--- src/services/user.ts
+++ src/services/user.ts
@@ -5,7 +5,7 @@ import type { UserSchema } from '#schemas/user';
 /**
  * Crea un usuario en la base de datos
  * @param data - datos del usuario que se va a crear
- * @returns - informacion del usuario creado
+ * @returns informacion del usuario creado
  * @example
  * await createUser({ name: Arnulfo, email: arnulfo@gmail.com, password: 1234 })
  */
@@ -16,7 +16,43 @@ export const create = async (data: UserSchema) => {
   return user;
 };
 
+/**
+ * Busca todos los usuarios de la base de datos
+ * @returns los usuarios listados de la base de datos
+ * @example
+ * await findMany()
+ * // returns - users: [{...}, {...}]
+ */
 export const findMany = async () => {
   const user = await prisma.user.findMany();
   return user;
+};
+
+/**
+ * Actualiza un usuario por su email
+ * @param email - Email del usuario
+ * @param data - Informacion a actualiza
+ * @returns nada
+ * @example
+ * await update('alejo@email.com', { name: 'carlos', ... })
+ */
+export const update = async (email: string, data: Partial<UserSchema>) => {
+  const user = await prisma.user.update({
+    where: { email },
+    data,
+  });
+  return user;
+};
+
+/**
+ * Realiza una operacion de eliminacion logica para el modelo User
+ * @param id id del registro
+ * @example
+ * await softDelete(2)
+ */
+export const softDelete = async (id: number) => {
+  await prisma.user.update({
+    where: { id },
+    data: { active: false },
+  });
 };
