5efdfd1892a92bd5f606895e5b741dd540b9d60b
Alejandro Molina <alejandro.ytf@gmail.com>
2026-01-19 16:16:43 -0500

    feat: servicio para creacion de sesiones y variables constantes

---
+++ src/services/session.ts
@@ -0,0 +1,21 @@
+import { prisma } from '#config/db';
+import { hashValue } from '#libs/bcrypt';
+import { SEVEN_DAYS } from '#utils/constants';
+import type { Prisma } from 'generated/prisma/client';
+
+export const create = async (
+  userId: number,
+  rawToken: string,
+): Promise<Prisma.SessionModel> => {
+  const hashedToken = await hashValue(rawToken);
+
+  const token = await prisma.session.create({
+    data: {
+      user: { connect: { id: userId } },
+      token: hashedToken,
+      expiresAt: new Date(Date.now() + SEVEN_DAYS),
+    },
+  });
+
+  return token;
+};
