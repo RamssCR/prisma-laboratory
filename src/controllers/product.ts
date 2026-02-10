8c7a37a8b9a3602bd7aa32182576a1525c6fbef5
Alejandro Molina <alejandro.ytf@gmail.com>
2026-02-10 09:29:35 -0500

    docs: documentacion controladores y servicios productos

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -9,6 +9,15 @@ import {
 import type { RequestHandler } from 'express';
 import status from 'http-status';
 
+/**
+ * Crea un producto en la base de datos
+ * @param req - Objeto de solicitud de Express
+ * @param res - Objeto de respuesta de Express
+ * @param next - Funcion para manejor de errores
+ * @returns Producto creado
+ * @example
+ * // Solicitud POST a /api/products
+ */
 export const createProduct: RequestHandler = async (req, res, next) => {
   try {
     const product = await create(req.body);
@@ -22,6 +31,15 @@ export const createProduct: RequestHandler = async (req, res, next) => {
   }
 };
 
+/**
+ * Devuelve todos los productos dependiendo del filtro
+ * @param req - Objeto de solicitud de Express
+ * @param res - Objeto de respuesta de Express
+ * @param next - Funcion para manejor de errores
+ * @returns Productos encontrados
+ * @example
+ * // Solicitud GET a /api/products || /api/products?search=yamaha
+ */
 export const getProducts: RequestHandler = async (req, res, next) => {
   try {
     const { search } = req.query;
@@ -36,6 +54,15 @@ export const getProducts: RequestHandler = async (req, res, next) => {
   }
 };
 
+/**
+ * Devuelve un producto por su id en la base de datos
+ * @param req - Objeto de solicitud de Express
+ * @param res - Objeto de respuesta de Express
+ * @param next - Funcion para manejor de errores
+ * @returns Producto encontrado
+ * @example
+ * // Solicitud GET a /api/products/1
+ */
 export const getProduct: RequestHandler = async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -50,6 +77,15 @@ export const getProduct: RequestHandler = async (req, res, next) => {
   }
 };
 
+/**
+ * Actualiza un producto por su id en la base de datos
+ * @param req - Objeto de solicitud de Express
+ * @param res - Objeto de respuesta de Express
+ * @param next - Funcion para manejor de errores
+ * @returns Producto actualizado
+ * @example
+ * // Solicitud PATCH a /api/products/1
+ */
 export const updateProduct: RequestHandler = async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -64,6 +100,15 @@ export const updateProduct: RequestHandler = async (req, res, next) => {
   }
 };
 
+/**
+ * Elimina un producto por su id en la base de datos
+ * @param req - Objeto de solicitud de Express
+ * @param res - Objeto de respuesta de Express
+ * @param next - Funcion para manejor de errores
+ * @returns Nada
+ * @example
+ * // Solicitud DELETE a /api/products/1
+ */
 export const deleteProduct: RequestHandler = async (req, res, next) => {
   try {
     const { id } = req.params;
--- src/services/product.ts
+++ src/services/product.ts
@@ -3,10 +3,24 @@ import type { ProductSchema } from '#schemas/product';
 import type { SearchQuery } from '#schemas/searchQuery';
 import slug from 'slug';
 
+/**
+ * Crea un producto en la base de datos
+ * @param data Datos de creacion
+ * @returns Producto creado
+ * @example
+ * await create({ name: "kit", sku: "2JH7KJ", price: 23000, minStock: 1, ...})
+ */
 export const create = (data: ProductSchema) =>
   !Array.isArray(data) &&
   prisma.product.create({ data: { ...data, slug: slug(data.name) } });
 
+/**
+ * Crea varios productos en la base de datos
+ * @param data Datos de creacion
+ * @returns Nada
+ * @example
+ * await createMany([{...}, {...}, ...])
+ */
 export const createMany = async (data: ProductSchema[]) => {
   if (Array.isArray(data)) {
     const slugData = data.map((item) => ({
@@ -20,12 +34,28 @@ export const createMany = async (data: ProductSchema[]) => {
   }
 };
 
+/**
+ * Actualiza un producto en la base de datos por su id
+ * @param id Id producto
+ * @param data Datos actualizacion
+ * @returns Prodcuto actualizado
+ * @example
+ * await update(1, { sku: 12JGG33 })
+ */
 export const update = (id: number, data: Partial<ProductSchema>) =>
   prisma.product.update({
     where: { id },
     data: data.name ? { ...data, slug: slug(data.name) } : data,
   });
 
+/**
+ * Devuelve todos los productos dependiendo del filtro de busqueda
+ * @param searchItem Input de busqueda
+ * @returns Productos encotrados
+ * @example
+ * await findMany("yamaha")
+ * // returns - data: [{...}, {...}]
+ */
 export const findMany = (searchItem?: SearchQuery['search']) =>
   prisma.product.findMany({
     where: {
@@ -52,6 +82,14 @@ export const findMany = (searchItem?: SearchQuery['search']) =>
     },
   });
 
+/**
+ * Devuelve un producto dependiendo del id dado
+ * @param id Id del producto
+ * @returns Producto encontrado
+ * @example
+ * await findUnique(1)
+ * // returns - data: { name: Kit arraste, ...}
+ */
 export const findUnique = (id: number) =>
   prisma.product.findUnique({
     where: { id },
@@ -61,5 +99,12 @@ export const findUnique = (id: number) =>
     },
   });
 
+/**
+ * Inactiva un producto dependiendo el id dado
+ * @param id Id del producto
+ * @returns Nada
+ * @example
+ * await softDelete(1)
+ */
 export const softDelete = (id: number) =>
   prisma.product.update({ where: { id }, data: { active: false } });
